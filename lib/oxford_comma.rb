0b27a8d838f2a96c58b18fbb6ef63565147588d5
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 02:55:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+  
+  size_of_array= array.length
+  new_string = array.join(" and ")
+  new_string2 = array.join( " , ")
+  
+    if size_of_array <= 2 
+    new_string
+    else
+    new_string2
+    
+  end
+end  
