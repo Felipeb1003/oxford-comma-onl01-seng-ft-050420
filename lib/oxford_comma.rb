e4e13364fc396c3770555390ac1d6307a243d52e
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 03:37:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+ require 'pry'
 def oxford_comma(array)
-
+  
+  size_of_array= array.length
+  new_string = array.join(" and ")
+  last_item = array.last
+  
+    if size_of_array <= 2 
+    new_string
+    else
+       last_item.insert(0, "and ")
+     array.join(", ")
+      
+     end
 end
+ 
