039de4edb423ee1ed05751c233300961b15b3cef
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 03:00:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
-
+ require 'pry'
 def oxford_comma(array)
   
   size_of_array= array.length
   new_string = array.join(" and ")
   new_string2 = array.join( " , ")
   last_item = array.last
-  
+  binding.pry
     if size_of_array <= 2 
     new_string
     else
