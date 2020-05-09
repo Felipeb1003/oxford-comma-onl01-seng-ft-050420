8af35b2512c954cbcfae254f7afffa81733e2b8f
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 02:55:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,11 +4,13 @@ def oxford_comma(array)
   size_of_array= array.length
   new_string = array.join(" and ")
   new_string2 = array.join( " , ")
+  last_item = array.last
   
     if size_of_array <= 2 
     new_string
     else
+      last_item  =  " and #{last_item}"   
     new_string2
     
   end
-end  
+end
