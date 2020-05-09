5194f3f1e6e06aea3d15908ff500ac36742c624d
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 03:27:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,14 +5,15 @@ def oxford_comma(array)
   size_of_array= array.length
   new_string = array.join(" and ")
   new_string2 = first_array.join( " , ")
-  last_item = "and" + " " + array.last
+  last_item = array.last
   
     if size_of_array <= 2 
     new_string
     else
-      first_array[-1] = last_item 
       new_string2 
+      last_item.insert (0, "and ")
     
     
   end
 end
+ first_array[-1] = last_item 
