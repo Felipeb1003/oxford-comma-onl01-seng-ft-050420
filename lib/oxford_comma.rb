146c816fbf0b3696113202f5f76bde86d36bfb74
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 03:28:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
  require 'pry'
 def oxford_comma(array)
   
-  first_array = array
   size_of_array= array.length
   new_string = array.join(" and ")
-  new_string2 = first_array.join( " , ")
+  new_string2 = array.join( " , ")
   last_item = array.last
   
     if size_of_array <= 2 
@@ -13,6 +12,6 @@ def oxford_comma(array)
       new_string2 
       last_item.insert (0, "and ")
     
-  end
+     end
 end
  
